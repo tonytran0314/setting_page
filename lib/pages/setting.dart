f13b530633a5ab4b410f10cff6fd3828c117f2d0
Trần Gia Huy <tonytran0314@gmail.com>
2025-04-04 19:42:55 -0400

    navigation

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -11,71 +11,343 @@ class Setting extends StatelessWidget {
       body: ListView(
         scrollDirection: Axis.vertical,
         children: [
-          Container(
-            margin: EdgeInsets.only(left: 16, right: 16),
-            padding: EdgeInsets.only(left: 18, right: 27, top: 18, bottom: 18),
-            decoration: BoxDecoration(
-              color: Color(0xff0601B4),
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xff000000).withOpacity(0.25),
-                  blurRadius: 4,
-                  spreadRadius: 1.0,
-                  offset: Offset(0, 4)
-                )
-              ]
-            ),
-            child: Row(
+          profileCard(),
+          SizedBox(height: 24),
+          navigation(),
+        ],
+      ),
+    );
+  }
+
+  Container navigation() {
+    return Container(
+          margin: EdgeInsets.only(left: 16, right: 16),
+          padding: EdgeInsets.only(left: 16, right: 16, top: 24, bottom: 24),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(6),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xff000000).withOpacity(0.1),
+                blurRadius: 20
+              )
+            ]
+          ),
+          child: Column(
+            children: [
+              myAccount(),
+              SizedBox(height: 24),
+              savedBeneficiary(),
+              SizedBox(height: 24),
+              faceIdOrToughId(),
+              SizedBox(height: 24),
+              twoFactorAuthentication(),
+              SizedBox(height: 24),
+              logout(),
+            ],
+          ),
+        );
+  }
+
+Row logout() {
+    return Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Row(
                   children: [
                     Container(
-                      width: 57,
-                      height: 57,
-                      padding: EdgeInsets.all(2),
+                      width: 40,
+                      height: 40,
+                      padding: EdgeInsets.all(8),
                       decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(50)
+                        color: Color(0xffF2F2FB),
+                        borderRadius: BorderRadius.circular(20)
                       ),
-                      child: Image.asset('assets/images/avatar.png')
+                      child: SvgPicture.asset('assets/icons/logout.svg')
                     ),
-                    SizedBox(width: 12),
+                    SizedBox(width: 16),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Itunuoluwa Abidoye',
+                          "Log out",
                           style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold
+                            color: Color(0xff181D27),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500
                           ),
                         ),
+                        SizedBox(height: 3),
                         Text(
-                          '@Itunuoluwa',
+                          "Logging out your account",
                           style: TextStyle(
-                            color: Color(0xffD7D7D7),
+                            color: Color(0xffABABAB),
                             fontSize: 12,
-                            fontWeight: FontWeight.normal
+                            fontWeight: FontWeight.w400
                           ),
                         )
                       ],
                     )
                   ],
                 ),
-                SvgPicture.asset(
-                  'assets/icons/edit.svg',
-                  width: 24,
-                  height: 24,
-                )
+                SvgPicture.asset('assets/icons/rightArrow.svg')
               ],
-            ),
-          )
-        ],
-      ),
-    );
+            );
+  }
+
+  Row twoFactorAuthentication() {
+    return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Color(0xffF2F2FB),
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: SvgPicture.asset('assets/icons/shield.svg')
+                    ),
+                    SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Two-Factor Authentication",
+                          style: TextStyle(
+                            color: Color(0xff181D27),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        SizedBox(height: 3),
+                        Text(
+                          "Further secure your account for safety",
+                          style: TextStyle(
+                            color: Color(0xffABABAB),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SvgPicture.asset('assets/icons/rightArrow.svg')
+              ],
+            );
+  }
+
+  Row faceIdOrToughId() {
+    return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Color(0xffF2F2FB),
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: SvgPicture.asset('assets/icons/lock.svg')
+                    ),
+                    SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Face ID / Touch ID",
+                          style: TextStyle(
+                            color: Color(0xff181D27),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        SizedBox(height: 3),
+                        Text(
+                          "Manage your device security",
+                          style: TextStyle(
+                            color: Color(0xffABABAB),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SvgPicture.asset('assets/icons/toggle.svg')
+              ],
+            );
+  }
+
+  Row savedBeneficiary() {
+    return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Color(0xffF2F2FB),
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: SvgPicture.asset('assets/icons/profile.svg')
+                    ),
+                    SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Saved Beneficiary",
+                          style: TextStyle(
+                            color: Color(0xff181D27),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        SizedBox(height: 3),
+                        Text(
+                          "Manage your saved account",
+                          style: TextStyle(
+                            color: Color(0xffABABAB),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SvgPicture.asset('assets/icons/rightArrow.svg')
+              ],
+            );
+  }
+
+  Row myAccount() {
+    return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Color(0xffF2F2FB),
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: SvgPicture.asset('assets/icons/profile.svg')
+                    ),
+                    SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "My Account",
+                          style: TextStyle(
+                            color: Color(0xff181D27),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        SizedBox(height: 3),
+                        Text(
+                          "Make changes to your account",
+                          style: TextStyle(
+                            color: Color(0xffABABAB),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset('assets/icons/warning.svg'),
+                    SizedBox(width: 32),
+                    SvgPicture.asset('assets/icons/rightArrow.svg')
+                  ],
+                ),
+              ],
+            );
+  }
+
+  Container profileCard() {
+    return Container(
+          margin: EdgeInsets.only(left: 16, right: 16),
+          padding: EdgeInsets.only(left: 18, right: 27, top: 18, bottom: 18),
+          decoration: BoxDecoration(
+            color: Color(0xff0601B4),
+            borderRadius: BorderRadius.circular(6),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xff000000).withOpacity(0.25),
+                blurRadius: 4,
+                spreadRadius: 1.0,
+                offset: Offset(0, 4)
+              )
+            ]
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 57,
+                    height: 57,
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Image.asset('assets/images/avatar.png')
+                  ),
+                  SizedBox(width: 12),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Itunuoluwa Abidoye',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text(
+                        '@Itunuoluwa',
+                        style: TextStyle(
+                          color: Color(0xffD7D7D7),
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              SvgPicture.asset(
+                'assets/icons/edit.svg',
+                width: 24,
+                height: 24,
+              )
+            ],
+          ),
+        );
   }
 
   AppBar appBar() {
